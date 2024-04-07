16be9c9bf14f3555f88d740dc6386a43e2c68803
Hilmy Setya Rahadian <87505460+instwo@users.noreply.github.com>
2024-04-07 18:29:59 +0700

    Delete doton.js

--- doton.js
+++
@@ -1,9 +0,0 @@
-// Add this inside your existing script section:
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
