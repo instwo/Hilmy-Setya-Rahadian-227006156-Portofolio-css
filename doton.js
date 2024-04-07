39c481d5cec00b4bcf05f1ec1371057127f2e2be
Hilmy Setya Rahadian <87505460+instwo@users.noreply.github.com>
2024-04-07 18:28:29 +0700

    Add files via upload

---
+++ doton.js
@@ -0,0 +1,9 @@
+// Add this inside your existing script section:
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
